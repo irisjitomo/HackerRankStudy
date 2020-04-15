7236695add6bcd660c754027912925eb8333553c
unknown <irisjitomo@yahoo.com>
2020-04-15 13:31:15 -0400

    bubble sort code

--- Section7-BubbleSort/Lesson2-BubbleSortOverview/bubbleSort.py
+++ Section7-BubbleSort/Lesson2-BubbleSortOverview/bubbleSort.py
@@ -1,14 +1,17 @@
 def bubbleSort(arr):
     for i in range(len(arr)):
-        for j in range(0, len(arr) - i - 1):
-            print(arr)
+        for j in range(0, len(arr) - i - 1): # this ensures that we only loop 
+            # - 1 after the pass in completed. So we never touch the sorted
+            # numbers on the right side of the list
+            print(arr, arr[j], arr[j+1])
             if arr[j] > arr[j+1]:
                 # temp = arr[j]
                 # arr[j] = arr[j+1]
                 # arr[j+1] = temp
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+        print("one pass completed")
     return arr
 
 
 
-print(bubbleSort([13,22,43,54,35,46,7]))
+print(bubbleSort([13,22,43,54,35,-46,7]))
