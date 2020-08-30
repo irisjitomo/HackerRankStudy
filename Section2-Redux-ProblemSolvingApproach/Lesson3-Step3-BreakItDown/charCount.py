a3265a2f0ac9063a32fb52c505cac1875b45f68d
unknown <irisjitomo@yahoo.com>
2020-08-29 21:10:55 -0400

    step 4 done

--- Section2-Redux-ProblemSolvingApproach/Lesson3-Step3-BreakItDown/charCount.py
+++ Section2-Redux-ProblemSolvingApproach/Lesson3-Step3-BreakItDown/charCount.py
@@ -4,13 +4,13 @@ def charCount(string):
     chars = dict()
     new_string = string.lower()
     for i in new_string:
-        if i.isspace() is False:
-            if i.isalnum() not in chars.keys():
+        if i.isspace() is False and i.isalnum() is True:
+            if i not in chars.keys():
                 chars[i] = 1
-            elif i.isalnum() in chars.keys():
+            elif i in chars.keys():
                 chars[i] += 1
     return chars
 
 
 
-print(charCount('Hello123   321 yes sir'))
+print(charCount('Hello123   321 yes sir!!!'))
--- Section2-Redux-ProblemSolvingApproach/Lesson4-Step4-Solve-The-Problem/notes.md
+++ Section2-Redux-ProblemSolvingApproach/Lesson4-Step4-Solve-The-Problem/notes.md
@@ -3,3 +3,11 @@ Step 4 - Solve the problem
 if you cant...
 
 Solve a Simpler Problem!
+
+
+SIMPLIFY 
+
+- Find the core difficulty in what youre trying to do
+- Temporarily ignore the difficulty
+- Write a simplified solution
+- Then incorporate that difficulty back in
